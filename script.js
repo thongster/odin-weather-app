68167ba3c36995e215a9e96cbf9d944cb3e06a67
thongster <106802301+thongster@users.noreply.github.com>
2025-11-05 17:59:34 +0700

    added humidity, wind, feelslike

--- script.js
+++ script.js
@@ -3,15 +3,20 @@
 const form = document.getElementById("searchForm")
 const search = document.getElementById("searchWeather")
 const button = document.getElementById("getWeatherButton")
-const icon = document.getElementById("icon")
-const temp = document.getElementById("temp")
-const description = document.getElementById("description")
+
+const resolvedAddress = document.getElementById("resolvedAddress")
+const condition = document.getElementById("condition")
+
 const date = document.getElementById("date")
 const time = document.getElementById("time")
 
-// inside display
-const resolvedAddress = document.getElementById("resolvedAddress")
-const condition = document.getElementById("condition")
+const icon = document.getElementById("icon")
+const temp = document.getElementById("temp")
+const description = document.getElementById("description")
+
+const humidity = document.getElementById("humidity")
+const wind = document.getElementById("wind")
+const feelslike = document.getElementById("feelslike")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -27,20 +32,23 @@ async function fetchData() {
         }
         const data = await response.json()
         console.log(data)
-        console.log(data.resolvedAddress)
+
         resolvedAddress.textContent = data.resolvedAddress
         condition.textContent = data.currentConditions.conditions
 
         time.textContent = data.currentConditions.datetime
-        console.log(data.currentConditions.datetime)
 
         // figure out icon import
         console.log(data.currentConditions.icon)
         temp.textContent = `${data.currentConditions.temp}°F`
         description.textContent = data.description
 
+        humidity.textContent = `${data.currentConditions.humidity}%`
+        wind.textContent = `${data.currentConditions.windspeed}km/h`
+        feelslike.textContent = `${data.currentConditions.feelslike}°F`
+        console.log(data.currentConditions.humidity)
         console.log(data.currentConditions.feelslike)
-        console.log(data.timezone, data.tzoffset)
+        console.log(data.currentConditions.windspeed)
 
         const tempF = data.currentConditions.temp
         const feelslikeF = data.currentConditions.feelslike
